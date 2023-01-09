ce6787e52bf927b036ca718cbeeed99aff7cdd6c
shin yeon lee <89951814+JisooKim0103@users.noreply.github.com>
2023-01-09 13:15:52 +0800

    Add files via upload

---
+++ KDTCLOUD/js/newlan.js
@@ -0,0 +1,7 @@
+// var myCarousel = document.getElementById('mycarousel')
+// var myCarousel = document.querySelector('#mycarousel')
+// // })
+// $('#mycarousel').carousel({
+//     interval: 3000,
+//     cycle: true
+//   }); 
